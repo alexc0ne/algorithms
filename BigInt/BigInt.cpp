dc4f1ca0374f5513f02596df069e8b972cef6f96
alexc0ne <alexnederlands@yandex.ru>
2021-09-28 21:30:26 +0200

    almost done +=

--- BigInt/BigInt.cpp
+++ BigInt/BigInt.cpp
@@ -1,8 +1,19 @@
 #include "BigInt.h"
+#include <iostream>
 #include <string>
 #include <algorithm>
 #include <sstream>
 
+
+void BigInt::updateIfNull()
+{
+    for (const auto & item : v_)
+        if (item != 0)
+            return;
+
+    sign_ = Sign::NON_NEGATIVE;
+    v_ = {0};
+}
 
 
 BigInt::BigInt() : sign_(Sign::NON_NEGATIVE), v_({0}) {}
@@ -11,24 +22,31 @@ BigInt::BigInt(const BigInt & other) : sign_(other.sign_), v_(other.v_) {}
 
 BigInt::BigInt(const std::string & s)
 {
-    // todo: --> check if:
-    //                     every char is between 0 and 9 except maybe s[0]
-    //                     len > 0
-    //
-    // later can be implemented using factory-method
+    // s is supposed to be here a valid string of type: "-yx..x" or "yx..x"
+    // where y is in [1-9]
+    //       x is in [0-9]
+    // todo: --> parse an s for validity
+    // todo: --> after parsing the string create separately an object in a (?) factory method
 
-    sign_ = (s[0] == '-') ? Sign::NEGATIVE : Sign::NON_NEGATIVE;
+    auto s_rend = s.rend();
+    if (s[0] == '-')
+    {
+        sign_ = Sign::NEGATIVE;
+        --s_rend;
+    }
+    else
+        sign_ = Sign::NON_NEGATIVE;
 
-    // todo: --> fix if s appears to be a negative number
-
-    v_.reserve(s.size());
-    for (auto it = s.rbegin(); it != s.rend(); ++it) v_.push_back(*it - '0');
+    v_.reserve(s_rend - s.rbegin());
+    for (auto it = s.rbegin(); it != s_rend; ++it)
+        v_.push_back(*it - '0');
 }
 
 
 BigInt::BigInt(int64_t i)
 {
     sign_ = i >= 0 ? Sign::NON_NEGATIVE : Sign::NEGATIVE;
+    i = std::abs(i);
 
     do
     {
@@ -42,8 +60,11 @@ BigInt::BigInt(int64_t i)
 std::string BigInt::toString() const
 {
     std::stringstream ss;
-    if (this->sign_ == Sign::NEGATIVE) ss << "-";
-    for (auto it = v_.rbegin(); it != v_.rend(); ++it) ss << *it;
+
+    if (this->sign_ == Sign::NEGATIVE)
+        ss << "-";
+    for (auto it = v_.rbegin(); it != v_.rend(); ++it)
+        ss << *it;
 
     return ss.str();
 }
@@ -67,4 +88,27 @@ bool BigInt::operator == (const BigInt & other)
         }
 
     return false;
+}
+
+
+const BigInt & BigInt::operator += (const BigInt &)
+{
+    // todo
+    return *this;
+}
+
+BigInt BigInt::operator + (const BigInt & other)
+{
+    BigInt res(*this);
+
+    res += other;
+    res.updateIfNull();
+
+    return res;
+}
+
+std::ostream & operator << (std::ostream & out, const BigInt & bi)
+{
+    out << bi.toString();
+    return out;
 }
--- BigInt/BigInt.h
+++ BigInt/BigInt.h
@@ -13,18 +13,36 @@ enum class Sign
 class BigInt
 {
 public:
-    BigInt();                                   // default ctor
-    BigInt(const std::string &);                // string ctor
-    BigInt(int64_t);                            // int ctor
-    BigInt(const BigInt &);                     // copy ctor
 
-    size_t size() const;                        // get size
-    std::string toString() const;               // string from BigInt
+    BigInt();                                       // default ctor
+    BigInt(const std::string &);                    // string ctor
+    BigInt(int64_t);                                // int ctor
+    BigInt(const BigInt &);                         // copy ctor
 
-    bool operator == (const BigInt &);          // operator ==
+    size_t size() const;                            // get size
+    std::string toString() const;                   // string from BigInt
+
+    bool operator == (const BigInt &);              // operator ==
+    //bool operator < (const BigInt &);               // operator ==
+    //bool operator <= (const BigInt &);              // operator ==
+    //bool operator > (const BigInt &);               // operator ==
+    //bool operator >= (const BigInt &);              // operator ==
+    //bool operator != (const BigInt &);              // operator ==
+
+
+    const BigInt & operator += (const BigInt &);    // operator +=
+    BigInt operator + (const BigInt &);
 
 private:
-    Sign sign_;                                 // sign of number
-    std::vector<int16_t> v_;                    // stores single digits, last digit in v_[0]
+
+    Sign sign_;                                     // sign of number
+    std::vector<int16_t> v_;                        // stores single digits, last digit in v_[0]
+
+    // perhaps make this method over
+    // from updateIfNull() to trimIfNull()    // {3,0,1,0,0,0} -- > {3,0,1}
+    void updateIfNull();                            // {0,...,0} --> {0}
 };
+
+
+std::ostream & operator << (std::ostream &, const BigInt &);    // operator <<
 
